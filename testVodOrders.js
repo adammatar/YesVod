8937dffab9093c9da8a186a2fbbd20448462a0fe
adammatar <60224240+adammatar@users.noreply.github.com>
2024-05-19 12:33:09 +0300

    Update testVodOrders.js
    Added more Records within the same customer ID and Genre to see records with discounts.

--- testVodOrders.js
+++ testVodOrders.js
@@ -9,15 +9,19 @@ const vodRecords = [
     "0238385,2024-05-01 17:54:23, NEWS,96737227,19.90",
     "0238322,2024-04-01 17:54:23, REALITY,96737227,29.90",
     "0238383,2024-05-01 18:54:23, COMEDY,5737227,0.90",
-    "0238383,2024-05-01 17:54:23, ACTION,137227,19.90"
+    "0238383,2024-05-01 17:54:23, ACTION,137227,19.90",
+    "0238383,2024-05-01 17:59:23, ACTION,137247,19.90",
+    "0238383,2024-05-01 17:01:23, ACTION,137217,19.90"
 ];
 
 const vodOrders = new VodOrders(vodRecords);
 
 // Test Method 1 ==> createTotalCustomerUsage  :)
 console.log("Testing Total Customer Usage:");
+console.log("customerId,monthYear,totalCharge,discount");
 vodOrders.createTotalCustomerUsage();
 
 // Test Method 2 ==> createSummaryReport
 console.log("\nTesting Summary Report:");
+console.log("monthYear,totalRevenue,totalOrders,totalDiscounts");
 vodOrders.createSummaryReport();
